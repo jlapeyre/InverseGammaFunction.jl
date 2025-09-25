ad61f78bba7beff3cdf485608cfc0b6f330d4f67
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-09-25 12:20:33 -0400

    Add content

--- src/InverseGammaFunction.jl
+++ src/InverseGammaFunction.jl
@@ -1,5 +1,33 @@
 module InverseGammaFunction
 
-# Write your package code here.
+using SpecialFunctions: gamma
+using Roots
 
+export invgamma
+
+"""
+    invgamma(z; branch=:hi)
+
+Return the inverse gamma function of `z`.
+
+`z` must be a real number greater than 0.885603194410888 and less than `gamma(171)`.
+If `branch` is `:hi`, then the returned value is in `(xmin, 171.0)` where `xmin=1.461632138429461`.
+If `branch` is `:lo`, then the returned value is in `(0, xmin)`.
+"""
+function invgamma(z::T; branch=:hi) where {T}
+    z >= 0.8856031944108887 ||
+        throw(ArgumentError(lazy"Argument must be > 0.885603194410888. Got $z."))
+    if branch == :hi
+        xmax = T(171)
+        xmin = T(3291302976991831//2251799813685248) # 1.461632138429461
+    elseif branch == :lo
+        xmax = T(3291302976991831//2251799813685248) # 1.461632138429461
+        xmin = zero(T)
+    else
+        throw(ArgumentError(lazy"Keyword argument `branch` must be one of `:hi` or `:lo`"))
+    end
+    f = x -> gamma(x) - z
+    find_zero(f, (xmin, xmax), Roots.Chandrapatla())
 end
+
+end # module InverseGammaFunction
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using InverseGammaFunction
 using Test
 
 @testset "InverseGammaFunction.jl" begin
-    # Write your tests here.
+    @test isapprox(invgamma(100.0), 5.892518696343773)
+    @test isapprox(invgamma(100.0; branch=:lo), 0.00994357323626315)
 end
