2660e31b213becdd3a977fdc3623977602b22cb5
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-09-25 12:20:33 -0400

    Add content

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # InverseGammaFunction
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://jlapeyre.github.io/InverseGammaFunction.jl/stable/)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://jlapeyre.github.io/InverseGammaFunction.jl/dev/)
-[![Build Status](https://github.com/jlapeyre/InverseGammaFunction.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/jlapeyre/InverseGammaFunction.jl/actions/workflows/CI.yml?query=branch%3Amain)
-[![Coverage](https://codecov.io/gh/jlapeyre/InverseGammaFunction.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/jlapeyre/InverseGammaFunction.jl)
-[![ColPrac: Contributor's Guide on Collaborative Practices for Community Packages](https://img.shields.io/badge/ColPrac-Contributor's%20Guide-blueviolet)](https://github.com/SciML/ColPrac)
+The package provides the inverse gamma function: `invgamma`.
+If simple calls a root finding function.
+
+This package is not registered.
--- src/InverseGammaFunction.jl
+++ src/InverseGammaFunction.jl
@@ -1,5 +1,33 @@
 module InverseGammaFunction
 
-# Write your package code here.
+using SpecialFunctions: gamma
+using Roots
 
+export invgamma
+
+"""
+    invgamma(z; branch=:hi)
+
+Return the inverse gamma function of `z`.
+
+`z` must be a real number greater than 0.885603194410888 and less than `gamma(171)`.
+If `branch` is `:hi`, then the returned value is in `(xmin, 171.0)` where `xmin=1.461632138429461`.
+If `branch` is `:lo`, then the returned value is in `(0, xmin)`.
+"""
+function invgamma(z; branch=:hi)
+    z >= 0.8856031944108887 ||
+        throw(ArgumentError(lazy"Argument must be > 0.885603194410888. Got $z."))
+    if branch == :hi
+        xmax = 171.0
+        xmin = 1.461632138429461
+    elseif branch == :lo
+        xmax = 1.461632138429461
+        xmin = 0.0
+    else
+        throw(ArgumentError(lazy"Keyword argument `branch` must be one of `:hi` or `:lo`"))
+    end
+    f = x -> gamma(x) - z
+    find_zero(f, (xmin, xmax), Roots.Chandrapatla())
 end
+
+end # module InverseGammaFunction
